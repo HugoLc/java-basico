547b80426c7ded239c910a791a99c949d11c8001
HugoLc <hugolindoso.c@gmail.com>
2023-01-30 21:32:01 -0300

    feat: mentoria e curso

---
+++ src/edu/dio/exercicio/bootcamp/Main.java
@@ -0,0 +1,11 @@
+package edu.dio.exercicio.bootcamp;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        Mentoria mentoria = new Mentoria();
+
+        System.out.println(curso);
+        System.out.println(mentoria);
+    }
+}
